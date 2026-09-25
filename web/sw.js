2854c7264e063925a18bd4435586501bd370d0ee
youbi41-ui <youbi41@gmail.com>
2026-09-25 12:29:29 +0300

    Publish GeoMark Web PWA with camera and GPS stamping

---
+++ web/sw.js
@@ -0,0 +1,10 @@
+const CACHE='geomark-web-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  const u=new URL(e.request.url);
+  if(u.hostname.includes('nominatim.openstreetmap.org')) return;
+  e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
